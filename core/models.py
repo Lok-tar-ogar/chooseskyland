7cf93769857ce1eaea98ddc15344d14322581413
macair <418586403@qq.com>
2016-09-06 11:19:45 +0800

    sss

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class NewsImg(models.Model):
-    Image = models.FileField('图片（496*360）', upload_to='img/')
+    Image = models.FileField('图片（496*360）', upload_to='core/static/img/')
     Linkto = models.CharField('链接地址（可为空）', max_length=50, blank=True)
     dimDate = models.DateTimeField(auto_now_add=True)  # timezone.now()
 
@@ -18,7 +18,7 @@ class NewsImg(models.Model):
 class Carousel(models.Model):
     """docstring for Carousel"""
     Title = models.CharField('标题', max_length=50)
-    Image = models.FileField('图片（1920*600）', upload_to='img/')
+    Image = models.FileField('图片（1920*600）', upload_to='core/static/img/')
     Linkto = models.CharField('链接地址（可为空）', max_length=50, blank=True)
     Caption = models.CharField('子标题', max_length=500, blank=True, null=True)
     dimDate = models.DateTimeField(auto_now_add=True)  # timezone.now()
@@ -33,18 +33,18 @@ class Carousel(models.Model):
 class Activities(models.Model):
     newsTitle = models.CharField('活动标题', max_length=50)
     upLoadImg1 = models.FileField(
-        '活动封面', blank=True, null=True, upload_to='img/')
+        '活动封面', blank=True, null=True, upload_to='core/static/img/')
     newsDetail = models.TextField('活动详情(图片可以直接粘贴)', max_length=10000)
     # obviously it is what it looks like.
 
     upLoadImg2 = models.FileField(
-        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     upLoadImg3 = models.FileField(
-        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     upLoadImg4 = models.FileField(
-        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     upLoadImg5 = models.FileField(
-        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传活动中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     viewedTimes = models.IntegerField('浏览次数',default=1)
     dimDate = models.DateTimeField(auto_now_add=True)  # timezone.now()
 
@@ -63,17 +63,17 @@ class Activities(models.Model):
 class News(models.Model):
     newsTitle = models.CharField('新闻标题', max_length=50)
     upLoadImg1 = models.FileField(
-        '活动封面', blank=True, null=True, upload_to='img/')
+        '活动封面', blank=True, null=True, upload_to='core/static/img/')
     newsDetail = models.TextField('新闻详情', max_length=10000)
     # obviously it is what it looks like.
     upLoadImg2 = models.FileField(
-        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     upLoadImg3 = models.FileField(
-        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     upLoadImg4 = models.FileField(
-        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     upLoadImg5 = models.FileField(
-        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='img/')
+        '上传新闻中的图片，引用图片请采用"/static/img/xxx.xx"格式', blank=True, null=True, upload_to='core/static/img/')
     viewedTimes = models.IntegerField('浏览次数',default=1)
     dimDate = models.DateTimeField(auto_now_add=True)  # timezone.now()
 
